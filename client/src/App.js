d8a86d46ee567e2758467d89e1b96a6085c0718a
Ritika Bhatt <ritikabhatt3110@gmail.com>
2021-09-24 13:51:20 +0530

    level one frontend completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
-
 import './App.css';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HomeScreen from './screens/HomeScreen';
+import ProductDetails from './screens/ProductDetails';
+import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <h1>Welcome to YOUR BAZAAR</h1>
-    </>
+    <Router>
+      <Header />
+      <main>
+        <Container>
+          {/* <h1>Welcome to YOUR BAZAAR</h1> */}
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductDetails} exact />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
