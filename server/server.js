09f2da93bceae1d3df397ca705a5b20fedfbb753
Ritika Bhatt <ritikabhatt3110@gmail.com>
2021-09-26 21:49:07 +0530

    server.js updated

--- server/server.js
+++ server/server.js
@@ -29,6 +29,10 @@ app.use("/api/users", usersRoutes);
 if(process.env.NODE_ENV === "production")
 {
     app.use(express.static("client/build"));
+    const path = require("path");
+    app.get("*",(req,res) =>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
 }
 
 
